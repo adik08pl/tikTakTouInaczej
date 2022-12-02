1d21c8baf4b00d3949690db48670f3ebc1a60859
Adik08 <adik08.eamil@gmail.com>
2022-12-02 18:56:51 +0100

    jest super ,dziala wreszcie

--- src/main/java/com/stempien/kolkoikrzyrzykalelepiej/HelloController.java
+++ src/main/java/com/stempien/kolkoikrzyrzykalelepiej/HelloController.java
@@ -5,9 +5,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class HelloController {
 
     public Button button;
+    public Label label;
 
     public Pane pPain;
 
@@ -16,12 +21,21 @@ public class HelloController {
         addButtons();
     }
 
+    boolean udaloSie = false;
+    int tura = 0;
+    ArrayList<Button> lista;
+
     private void addButtons() {
-        //int x = 0;
+        label = new Label();
+        label.setPrefSize(300, 100);
+        label.setLayoutX(400);
+        label.setLayoutY(100);
+        pPain.getChildren().add(label);
         button = new Button();
         int y = 0;
+        lista = new ArrayList<>();
         for (int j = 0; j < 3; j++) {
-            int x=0;
+            int x = 0;
 
             for (int i = 0; i < 3; i++) {
                 button = new Button();
@@ -29,12 +43,10 @@ public class HelloController {
                 button.setLayoutX(x);
                 button.setLayoutY(y);
                 pPain.getChildren().add(button);
-                System.out.println("x" + i + x);
                 x += 100;
                 button.setDisable(true);
-
+                lista.add(button);
             }
-            System.out.println("y" + j + y);
             y += 100;
         }
         button = new Button();
@@ -43,5 +55,51 @@ public class HelloController {
         button.setLayoutY(200);
         button.setText("Graj");
         pPain.getChildren().add(button);
+        Random random = new Random();
+        button.setOnAction(event -> {
+            String znak;
+            do {
+                udaloSie = false;
+                tura++;
+                if (tura % 2 == 1)
+                    znak = "X";
+                else
+                    znak = "O";
+                int losowyPrzycisk = random.nextInt(9);
+                if (lista.get(losowyPrzycisk).getText().equals("")) {
+                    lista.get(losowyPrzycisk).setText(znak);
+                    udaloSie = true;
+                } else
+                    tura--;
+                czyWygral();
+            } while (!udaloSie);
+        });
+    }
+
+    public void czyWygral() {
+        if (lista.get(0).getText().equals("O") && lista.get(1).getText().equals("O") && lista.get(2).getText().equals("O") || lista.get(3).getText().equals("O") && lista.get(4).getText().equals("O") && lista.get(5).getText().equals("O") || lista.get(6).getText().equals("O") && lista.get(7).getText().equals("O") && lista.get(8).getText().equals("O") ||
+                lista.get(0).getText().equals("O") && lista.get(4).getText().equals("O") && lista.get(8).getText().equals("O") || lista.get(2).getText().equals("O") && lista.get(4).getText().equals("O") && lista.get(6).getText().equals("O") ||
+                lista.get(0).getText().equals("O") && lista.get(3).getText().equals("O") && lista.get(6).getText().equals("O") || lista.get(1).getText().equals("O") && lista.get(4).getText().equals("O") && lista.get(7).getText().equals("O") || lista.get(2).getText().equals("O") && lista.get(5).getText().equals("O") && lista.get(8).getText().equals("O")) {
+            tura = 0;
+            Stream.of(lista.get(0), lista.get(1), lista.get(2), lista.get(3), lista.get(4), lista.get(5), lista.get(6), lista.get(7), lista.get(8)).forEach(btn -> {
+                btn.setText("");
+            });
+            label.setText("Wygrywają kółka");
+
+        } else if (lista.get(0).getText().equals("X") && lista.get(1).getText().equals("X") && lista.get(2).getText().equals("X") || lista.get(3).getText().equals("X") && lista.get(4).getText().equals("X") && lista.get(5).getText().equals("X") || lista.get(6).getText().equals("X") && lista.get(7).getText().equals("X") && lista.get(8).getText().equals("X") ||
+                lista.get(0).getText().equals("X") && lista.get(4).getText().equals("X") && lista.get(8).getText().equals("X") || lista.get(2).getText().equals("X") && lista.get(4).getText().equals("X") && lista.get(6).getText().equals("X") ||
+                lista.get(0).getText().equals("X") && lista.get(3).getText().equals("X") && lista.get(6).getText().equals("X") || lista.get(1).getText().equals("X") && lista.get(4).getText().equals("X") && lista.get(7).getText().equals("X") || lista.get(2).getText().equals("X") && lista.get(5).getText().equals("X") && lista.get(8).getText().equals("X")) {
+            tura = 0;
+            Stream.of(lista.get(0), lista.get(1), lista.get(2), lista.get(3), lista.get(4), lista.get(5), lista.get(6), lista.get(7), lista.get(8)).forEach(btn -> {
+                btn.setText("");
+            });
+            label.setText("Wygrywają krzyżyki");
+        }
+        else if(tura == 9){
+            Stream.of(lista.get(0), lista.get(1), lista.get(2), lista.get(3), lista.get(4), lista.get(5), lista.get(6), lista.get(7), lista.get(8)).forEach(btn -> {
+                btn.setText("");
+            });
+            label.setText("Remis");
+        }
     }
 }
